b97f1af7879b56a9b4ba6a7a1da64381d53aab66
Cooper b. Anderson <andersc7@rose-hulman.edu>
2021-01-20 23:45:59 -0500

    Add directedGraph stub files

---
+++ src/benzer/__tests__/directedGraph.test.ts
@@ -0,0 +1,18 @@
+import DirectedGraph from "../directedGraph";
+
+describe("DirectedGraph", () => {
+	let dg: DirectedGraph;
+
+	it("gets created successfully", () => {
+		dg = new DirectedGraph();
+	});
+
+	it("assigns edge directions", () => {
+
+	});
+
+	it("has the same number of edges", () => {
+
+	});
+});
+
